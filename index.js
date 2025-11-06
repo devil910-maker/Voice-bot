2954477059557258141076cc20f27956a6b8f42d
devil910-maker <devil910@naver.com>
2025-11-06 13:36:52 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Bot Ready");
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Bot Ready");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
